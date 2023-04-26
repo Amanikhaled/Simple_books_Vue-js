1e4ce7255322bec407656bafeaedc6127ca7716f
Amani khaled <Amani.khaled.mohamed@gmail.com>
2023-04-26 15:06:47 +0200

    lab1

---
+++ book.js
@@ -0,0 +1,131 @@
+books = [
+  {
+    ISBN: "001",
+    author: "Chinua Achebe",
+
+    imageLink:
+      "https://th.bing.com/th/id/R.8546820ee580a8cdb3a9c1ebd11a4daa?rik=4fwMklQJCQU1Ow&riu=http%3a%2f%2fredonline.cdnds.net%2fmain%2fthumbs%2f25788%2fstack_of_books.jpg&ehk=xwtWWiDiDhhxV8yXIhXFgeNFvZ4ETt83uVe2km2jabI%3d&risl=&pid=ImgRaw&r=0",
+    price: 1000,
+    pages: 9,
+    title: "Things Fall Apart",
+    category: "sience",
+  },
+  {
+    ISBN: "002",
+    author: "Hans Christian Andersen",
+
+    imageLink:
+      "https://th.bing.com/th/id/R.8546820ee580a8cdb3a9c1ebd11a4daa?rik=4fwMklQJCQU1Ow&riu=http%3a%2f%2fredonline.cdnds.net%2fmain%2fthumbs%2f25788%2fstack_of_books.jpg&ehk=xwtWWiDiDhhxV8yXIhXFgeNFvZ4ETt83uVe2km2jabI%3d&risl=&pid=ImgRaw&r=0",
+    price: 2000,
+    pages: 784,
+    title: "Fairy tales",
+  },
+  {
+    ISBN: "003",
+    author: "Dante Alighieri",
+    country: "Italy",
+    imageLink:
+      "https://th.bing.com/th/id/R.8546820ee580a8cdb3a9c1ebd11a4daa?rik=4fwMklQJCQU1Ow&riu=http%3a%2f%2fredonline.cdnds.net%2fmain%2fthumbs%2f25788%2fstack_of_books.jpg&ehk=xwtWWiDiDhhxV8yXIhXFgeNFvZ4ETt83uVe2km2jabI%3d&risl=&pid=ImgRaw&r=0",
+    language: "Italian",
+    link: "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+    pages: 928,
+    title: "The Divine Comedy",
+    year: 1315,
+    category: "fantasy",
+    price: 1500,
+  },
+  {
+    ISBN: "004",
+    author: "Unknown",
+    country: "Sumer and Akkadian Empire",
+    imageLink:
+      "https://th.bing.com/th/id/R.8546820ee580a8cdb3a9c1ebd11a4daa?rik=4fwMklQJCQU1Ow&riu=http%3a%2f%2fredonline.cdnds.net%2fmain%2fthumbs%2f25788%2fstack_of_books.jpg&ehk=xwtWWiDiDhhxV8yXIhXFgeNFvZ4ETt83uVe2km2jabI%3d&risl=&pid=ImgRaw&r=0",
+    language: "Akkadian",
+    link: "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
+    pages: 160,
+    title: "The Epic Of Gilgamesh",
+    year: 1700,
+    category: "fantasy",
+    price: 5500,
+  },
+  {
+    ISBN: "005",
+    author: "Unknown",
+    country: "Achaemenid Empire",
+    imageLink:
+      "https://th.bing.com/th/id/R.8546820ee580a8cdb3a9c1ebd11a4daa?rik=4fwMklQJCQU1Ow&riu=http%3a%2f%2fredonline.cdnds.net%2fmain%2fthumbs%2f25788%2fstack_of_books.jpg&ehk=xwtWWiDiDhhxV8yXIhXFgeNFvZ4ETt83uVe2km2jabI%3d&risl=&pid=ImgRaw&r=0",
+    language: "Hebrew",
+    link: "https://en.wikipedia.org/wiki/Book_of_Job\n",
+    pages: 176,
+    title: "The Book Of Job",
+    year: -600,
+    category: "fantasy",
+    price: 1500,
+  },
+  {
+    ISBN: "006",
+    author: "Unknown",
+    country: "India/Iran/Iraq/Egypt/Tajikistan",
+    imageLink:
+      "https://th.bing.com/th/id/R.8546820ee580a8cdb3a9c1ebd11a4daa?rik=4fwMklQJCQU1Ow&riu=http%3a%2f%2fredonline.cdnds.net%2fmain%2fthumbs%2f25788%2fstack_of_books.jpg&ehk=xwtWWiDiDhhxV8yXIhXFgeNFvZ4ETt83uVe2km2jabI%3d&risl=&pid=ImgRaw&r=0",
+    language: "Arabic",
+    link: "https://en.wikipedia.org/wiki/One_Thousand_and_One_Nights\n",
+    pages: 288,
+    title: "One Thousand and One Nights",
+    year: 1200,
+    category: "fantasy",
+    price: 1500,
+  },
+  {
+    ISBN: "007",
+    author: "Unknown",
+    country: "Iceland",
+    imageLink:
+      "https://th.bing.com/th/id/R.8546820ee580a8cdb3a9c1ebd11a4daa?rik=4fwMklQJCQU1Ow&riu=http%3a%2f%2fredonline.cdnds.net%2fmain%2fthumbs%2f25788%2fstack_of_books.jpg&ehk=xwtWWiDiDhhxV8yXIhXFgeNFvZ4ETt83uVe2km2jabI%3d&risl=&pid=ImgRaw&r=0",
+    language: "Old Norse",
+    link: "https://en.wikipedia.org/wiki/Nj%C3%A1ls_saga\n",
+    pages: 384,
+    title: "Nj\u00e1l's Saga",
+    year: 1350,
+    category: "fantasy",
+    price: 1500,
+  },
+  {
+    ISBN: "008",
+    author: "Jane Austen",
+    country: "United Kingdom",
+    imageLink:
+      "https://th.bing.com/th/id/R.8546820ee580a8cdb3a9c1ebd11a4daa?rik=4fwMklQJCQU1Ow&riu=http%3a%2f%2fredonline.cdnds.net%2fmain%2fthumbs%2f25788%2fstack_of_books.jpg&ehk=xwtWWiDiDhhxV8yXIhXFgeNFvZ4ETt83uVe2km2jabI%3d&risl=&pid=ImgRaw&r=0",
+    language: "English",
+    link: "https://en.wikipedia.org/wiki/Pride_and_Prejudice\n",
+    pages: 226,
+    title: "Pride and Prejudice",
+    year: 1813,
+    category: "fantasy",
+    price: 1500,
+  },
+  {
+    ISBN: "009",
+    author: "Honor\u00e9 de Balzac",
+    country: "France",
+    imageLink:
+      "https://th.bing.com/th/id/R.8546820ee580a8cdb3a9c1ebd11a4daa?rik=4fwMklQJCQU1Ow&riu=http%3a%2f%2fredonline.cdnds.net%2fmain%2fthumbs%2f25788%2fstack_of_books.jpg&ehk=xwtWWiDiDhhxV8yXIhXFgeNFvZ4ETt83uVe2km2jabI%3d&risl=&pid=ImgRaw&r=0",
+    language: "French",
+    link: "https://en.wikipedia.org/wiki/Le_P%C3%A8re_Goriot\n",
+    pages: 443,
+    title: "Le P\u00e8re Goriot",
+    category: "fantasy",
+    price: 1500,
+  },
+  {
+    ISBN: "010",
+    author: "Samuel Beckett",
+    category: "fantasy",
+    price: 1500,
+    imageLink:
+      "https://th.bing.com/th/id/R.8546820ee580a8cdb3a9c1ebd11a4daa?rik=4fwMklQJCQU1Ow&riu=http%3a%2f%2fredonline.cdnds.net%2fmain%2fthumbs%2f25788%2fstack_of_books.jpg&ehk=xwtWWiDiDhhxV8yXIhXFgeNFvZ4ETt83uVe2km2jabI%3d&risl=&pid=ImgRaw&r=0",
+
+    pages: 256,
+    title: "Molloy, Malone Dies, The Unnamable, the trilogy",
+  },
+];
